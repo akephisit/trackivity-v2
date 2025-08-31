98ae841737f6588e78423e0a9aa735412919c74a
akephisit <kruakemaths@gmail.com>
2025-08-31 07:04:23 +0700

    Revert "แก้ error"
    This reverts commit 2d9d94f88bfc4c2c9cbc68a26afdc1368c058dff.

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -2,10 +2,9 @@ import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 import * as schema from './schema';
 import { env } from '$env/dynamic/private';
-import { building } from '$app/environment';
-// Create connection; avoid throwing during SvelteKit build
-const connectionString = env.DATABASE_URL || (building ? 'postgres://user:pass@localhost:5432/dummy' : '');
-if (!connectionString && !building) {
+// Create connection
+const connectionString = env.DATABASE_URL;
+if (!connectionString) {
     throw new Error('DATABASE_URL is not set in environment');
 }
 const client = postgres(connectionString, {
